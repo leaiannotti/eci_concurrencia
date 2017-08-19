023169d204b4cfbdbfb1cf35337a376266c5e421
Marcelo <marcelo@MacBook-Pro-3.local>
2017-08-18 21:14:21 -0300

    DEKKER cpp andando joya

--- dekker_cpp/Dekker.cpp
+++ dekker_cpp/Dekker.cpp
@@ -10,24 +10,26 @@
 using namespace std;
 
 int tes;
-int Id; /* Segment Id */
-volatile int *TURN;
+int Id;
+int M_TURN;
+int M_FLAG_0;
+int M_FLAG_1;
+
+atomic<int> *TURN;
 atomic<bool> *FLAG_0;
 atomic<bool> *FLAG_1;
-
-// atomic_flag flag_atomic = ATOMIC_FLAG_INIT;
 
 void rel_mutex(int i)
 {
    if(i==0){
-      *TURN=1;
+      TURN->store(1,std::memory_order_seq_cst);
       FLAG_0->store(false,std::memory_order_seq_cst);
    }
    if(i==1){
-      *TURN=0;
+      TURN->store(0,std::memory_order_seq_cst);
       FLAG_1->store(false,std::memory_order_seq_cst);
    }
-
+	sleep(1);
 }
 
 
@@ -35,36 +37,36 @@ void get_mutex(int i)
 {
 
    if(i==0){
-      cout<<"Pidiendo el mutex: 0 - 1"<<endl;
+
       FLAG_0->store(true,memory_order_seq_cst);
-            cout<<"Pidiendo el mutex: 0 - 2"<<endl;
+       //     cout<<"Pidiendo el mutex: 0 - 2"<<endl;
       while(FLAG_1->load(memory_order_seq_cst)){
-            cout<<"Pidiendo el mutex: 0 - 3"<<endl;
-           if(*TURN==1){
-                 cout<<"Pidiendo el mutex: 0 - 4"<<endl;
+         //   cout<<"Pidiendo el mutex: 0 - 3"<<endl;
+           if(TURN->load(memory_order_seq_cst)==1){
+           //      cout<<"Pidiendo el mutex: 0 - 4"<<endl;
             FLAG_0->store(false,memory_order_seq_cst);
-                  cout<<"Pidiendo el mutex: 0 - 5"<<endl;
-            while(*TURN==1){}
-                  cout<<"Pidiendo el mutex: 0 - 6"<<endl;
+             //     cout<<"Pidiendo el mutex: 0 - 5"<<endl;
+            while(TURN->load(memory_order_seq_cst)==1){}
+               //   cout<<"Pidiendo el mutex: 0 - 6"<<endl;
             FLAG_0->store(true,memory_order_seq_cst);
-                  cout<<"Pidiendo el mutex: 0 - 7"<<endl;
+                 // cout<<"Pidiendo el mutex: 0 - 7"<<endl;
          }
       }
    }
    if(i==1){
-         cout<<"Pidiendo el mutex: 1 - 1"<<endl;
+       //  cout<<"Pidiendo el mutex: 1 - 1"<<endl;
       FLAG_1->store(true,memory_order_seq_cst);
-            cout<<"Pidiendo el mutex: 1 - 2"<<endl;
+         //   cout<<"Pidiendo el mutex: 1 - 2"<<endl;
       while (FLAG_0->load(memory_order_seq_cst)){
-            cout<<"Pidiendo el mutex: 1 - 3"<<endl;
-         if(*TURN==0){
-               cout<<"Pidiendo el mutex: 1 - 4"<<endl;
+           // cout<<"Pidiendo el mutex: 1 - 3"<<endl;
+         if(TURN->load(memory_order_seq_cst)==0){
+             //  cout<<"Pidiendo el mutex: 1 - 4"<<endl;
             FLAG_1->store(false,memory_order_seq_cst);
-                  cout<<"Pidiendo el mutex: 1 - 5"<<endl;
-            while(*TURN==0){}
-                  cout<<"Pidiendo el mutex: 1 - 6"<<endl;
+               //   cout<<"Pidiendo el mutex: 1 - 5"<<endl;
+            while(TURN->load(memory_order_seq_cst)==0){}
+                 // cout<<"Pidiendo el mutex: 1 - 6"<<endl;
             FLAG_1->store(true,memory_order_seq_cst);
-                  cout<<"Pidiendo el mutex: 1 - 7"<<endl;
+                 // cout<<"Pidiendo el mutex: 1 - 7"<<endl;
          }
       }
    }
@@ -90,17 +92,10 @@ void process(int i)
 }
 
 
-/*
- System.out.println("Running: "+tid);
-            for (int s = 0; s < PASOS; s++) {
-              m.getMutex(tid);
-              c.increment();
-              m.relMutex(tid);
-            }*/
 
 void del(int sig)
 {
-   (void) shmdt((char *) TURN);
+   (void) shmdt((char *) &TURN);
    (void) shmdt((char *) &FLAG_0);
    (void) shmdt((char *) &FLAG_1);
    (void) shmctl(Id, IPC_RMID, NULL);
@@ -115,12 +110,18 @@ int main()
    if (Id == -1)
       exit(1);
 
-   TURN = (int *) shmat(Id, NULL, 0);
-   *TURN = 0;
-   FLAG_0 = (atomic<bool>*) shmat(Id, NULL, 0);
-   FLAG_1 = (atomic<bool>*) shmat(Id, NULL, 0);
+   M_TURN = shmget(IPC_PRIVATE, sizeof(int), 0600);
+   TURN = (atomic<int>*) shmat(M_TURN, NULL, 0);
+   TURN->store(0,memory_order_seq_cst);
+   
+   M_FLAG_0 = shmget(IPC_PRIVATE, sizeof(bool), 0600);
+   FLAG_0 = (atomic<bool>*) shmat(M_FLAG_0, NULL, 0);
    FLAG_0->store(false,memory_order_seq_cst);
+   
+   M_FLAG_1 = shmget(IPC_PRIVATE, sizeof(bool), 0600);
+   FLAG_1 = (atomic<bool>*) shmat(M_FLAG_1, NULL, 0);
    FLAG_1->store(false,memory_order_seq_cst);
+   
    sigset(SIGINT, del);
 
    pid_t pid = fork();
